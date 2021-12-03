6b728edab16eff1df09b31d7209338f9b34d2a93
Petra Carrion <petra@carrion.io>
2021-12-03 21:50:19 +0100

    Solve day 01 part 02

--- day-01/day-01-part-02.py
+++ day-01/day-01-part-02.py
@@ -4,12 +4,18 @@ def main():
         lines = [line.rstrip() for line in lines]
     numbers = [int(line) for line in lines]
 
+    groups = []
+    for idx in range(len(numbers) - 2):
+        groups.append(numbers[idx] + numbers[idx + 1] + numbers[idx + 2])
+    print(groups)
+
     counter = 0
-    previous_number = numbers.pop(0)
-    for n in numbers:
+    previous_number = groups.pop(0)
+    for n in groups:
         if n > previous_number:
             counter += 1
         previous_number = n
+
     print(counter)
 
 
