cec3dbd5339c1c3b47cbe729cb68113908e3a54f
Petra Carrion <petra@carrion.io>
2021-12-04 13:21:54 +0100

    Update README.md

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # Advent of Code 2021
 
-
-* day-1/
-  * [day-01-part-01.md](day-01/day-01-part-01.py)
+* [day-01](day-01)
+    * [day-01-part-01.py](day-01/day-01-part-01.py)
+    * [day-01-part-02.py](day-01/day-01-part-02.py)
+* [day-02](day-02)
+    * [day-02-part-01.py](day-02/day-02-part-01.py)
+    * [day-02-part-02.py](day-02/day-02-part-02.py)
+* [day-03](day-03)
+    * [day-03-part-01.py](day-03/day-03-part-01.py)
+    * [day-03-part-02.py](day-03/day-03-part-02.py)
---
+++ common/update-readme.py
@@ -0,0 +1,26 @@
+import glob
+import os
+
+
+def main():
+    lines = ['# Advent of Code 2021', '']
+
+    os.chdir('..')
+    files = glob.glob('day**/*.py', recursive=True)
+    files = sorted(files)
+    files = [file.split('/') for file in files]
+    last_file = ['', '']
+    for file in files:
+        if file[0] != last_file[0]:
+            lines.append(f'* [{file[0]}]({file[0]})')
+        lines.append(f'    * [{file[1]}]({file[0]}/{file[1]})')
+        last_file = file
+
+    print(lines)
+    with open('README.md', 'w') as f:
+        for line in lines:
+            f.write("%s\n" % line)
+
+
+if __name__ == '__main__':
+    main()
